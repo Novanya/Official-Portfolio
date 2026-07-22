4ca7d8fb373e52d3bb70c0420cf7075c34144b3a
Novanya Mendis <novamendis@gmail.com>
2026-07-22 13:50:14 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,7 @@ import "./Education.css";
 export default function Education() {
   const education = [
     {
-      period: "2024 - 2025",
+      period: "2025 - Present",
       degree: "Bachelor of AI & Data Science",
       institution: "Robert Gordon University, Aberdeen, UK",
       desc: "Currently pursuing the Bachelor's degree in AI and Data Science, covering programming, machine learning basics, and web development projects.",
@@ -24,6 +24,24 @@ export default function Education() {
       institution: "University of Westminster",
       desc: "Completed foundational computing courses including programming logic, data structures, and basic web development.",
       badges: ["Python", "Data Base(SQL)", "Maths for Computing", "SDLC"],
+    },
+    {
+      period: "2025",
+      degree: "100 Days of Code Bootcamp",
+      institution: "Angela Yu",
+      desc: "A structured 100-day Python learning journey focused on strengthening programming and creating practical applications including automation scripts, web applications, data projects, and interactive programs.",
+      badges:[
+    "Python",
+    "Object-Oriented Programming (OOP)",
+    "Automation",
+    "Web Scraping",
+    "API Integration",
+    "Flask",
+    "Data Processing",
+    "SQL",
+    "Selenium",
+    "Git & Version Control"
+],
     },
   ];
 
